d144f0d0b128e84afe3efb8c4e35eb5fddc8fe8a
Ashes Das <dasashes04@gmail.com>
2025-02-14 11:02:42 +0530

    basic set up done

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from 'express';
-
-
+import { connectDB } from './connectDB.js';
+import { configDotenv } from 'dotenv';
 const app = new express();
-
+configDotenv();
 const PORT  = process.env.PORT || 3000;
-app.listen(3000 , ()=>{
+app.listen(PORT , ()=>{
     console.log(`server is running on port ${PORT}`);
 });
 
+
+app.get('/' , (req,res)=>{
+    res.send('hello world');
+})
+
+connectDB();
